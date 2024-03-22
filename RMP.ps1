d0008fdd5c84aee38b1f81441cc6c02c1bd77926
Ikkxeer <137718457+ikkxeer@users.noreply.github.com>
2024-03-22 09:31:30 +0100

    Update RMP.ps1

--- RMP.ps1
+++ RMP.ps1
@@ -10,30 +10,40 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
   Break
 }
 
+# Function to print space in the terminal
 function Space {
     Write-Host " "    
 }
+
+# Function to the main menu
 function MainMenu {
     Write-Host @"
-  _______     ____    ____  _______   
- |_   __ \   |_   \  /   _||_   __ \  
+  _______    ____    ____   _______   
+ |_   __ \  |_   \  /   _| |_   __ \  
   | |__) |    |   \/   |    | |__) | 
   |  __ /     | |\  /| |    |  ___/  
  _| |  \ \_  _| |_\/_| |_  _| |_     
 |____| |___||_____||_____||_____|       
 "@ -ForegroundColor Yellow
 Space
-$Hostname = Read-Host "Hostname or Ip to the host: "
+$global:Hostname = Read-Host "Hostname or Ip to the host: "
 Space
 $TestPSRemote = Test-WSMan $Hostname -ErrorAction SilentlyContinue
-if ($TestPSRemote) {
-    Write-Host "The connection with $Hostname was successfully!" -ForegroundColor Green
-}
-else {
-    Write-Host "$Hostname do not have PS Remote enabled correctly or the remote configuration is not correct" -ForegroundColor Red
-    Space
-    Pause
-}
+    if ($TestPSRemote) {
+        Write-Host "The connection with $Hostname was successfully!" -ForegroundColor Green
+        Space
+        Write-Host "Getting credentials to the connection with the machine: $Hostname" -ForegroundColor Yellow
+        Space
+        $global:Credentials = Get-credentials
+        Space
+        Write-Host "Credentials have been successfully obtained!" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$Hostname do not have PS Remote enabled correctly or the remote configuration is not correct" -ForegroundColor Red
+        Space
+        Start-Sleep -Seconds 2
+        exit
+    }
 }
 
 MainMenu
